49f5c9236cacd381f347a458a853e314439e3222
Sanjida Khanam <sanjida.ice.778@gmail.com>
2025-06-02 12:23:34 +0600

    use aliases

--- esm/file1.mjs
+++ esm/file1.mjs
@@ -1,4 +1,8 @@
 import { a, add } from "./file2.mjs";
+import { a as A, add as ADD } from "./file3.mjs";
 console.log(a);
 const b = add(2, 3);
 console.log(b);
+
+console.log(A);
+console.log(ADD(4, 5, 6));
